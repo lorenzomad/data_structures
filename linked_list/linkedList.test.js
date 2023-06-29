e91cb65b9b6d7a4c81753a6d6b9171602bb1245b
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-06-29 10:11:45 +0000

    added prepend test and function

--- linked_list/linkedList.test.js
+++ linked_list/linkedList.test.js
@@ -26,3 +26,16 @@ test('append second node check first 2 values', () => {
     expect(testList.listHead.nextNode.value).toBe(2)
         
 });
+
+test('prepend when there is no node', () => {    
+    testList.prepend(4)
+    expect(testList.listHead.value).toBe(4)
+});
+
+test('prepend when first node already exists', () => {
+    testList.listHead = Node(4)
+    testList.prepend(2)
+    expect(testList.listHead.value).toBe(2)
+    expect(testList.listHead.nextNode.value).toBe(4)
+        
+});
